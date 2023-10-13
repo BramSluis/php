d44311d2c932ea74cfc0dc6b099c2eb994e00239
superdoggo83 <bramvandersluis83@gmail.com>
2023-10-13 18:36:28 +0200

    htmlbasis

---
+++ htmlcss/tekstlinks/tekstlinks.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="tekstlinks.css" type="text/css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+    <h1> Random Facts </h1>
+    </form>
+
+<p> Did you know a gram of uranium is about 20 <b>billion</b> calories?</p>
+<p> Did you know <i>bees</i> are capable of percieving time?</p>
+</body>
+</html>
