d0d51554a7b053c51986aa5dfdd35e3998b1f5ba
superdoggo83 <bramvandersluis83@gmail.com>
2023-10-16 13:31:11 +0200

    1.1

---
+++ htmlcss/tekstlinks/submap/submaptest.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> Als je het goed hebt gedaan zie je mij!!</p>
+</body>
+</html>
--- htmlcss/tekstlinks/tekstlinks.php
+++ htmlcss/tekstlinks/tekstlinks.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="tekstlinks.css" type="text/css">
+   <link rel="stylesheet" href="tekstlinks.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <form>
+    
     <h1> Random Facts </h1>
-    </form>
+    
 
 <p> Did you know a gram of uranium is about 20 <b>billion</b> calories?</p>
 <p> Did you know <i>bees</i> are capable of percieving time?</p>
+<p> Did you know the US air force made a supercomputer out of 1760 ps3 consoles? nowadays that would cost about &euro;90000</p>
+<p> <a href="submap/submaptest.php">Link naar bestand in submap</a> </p>
 </body>
 </html>
