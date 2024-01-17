bc8d792e0eb4bae545e4ae3d9601e4aacaf8b1e7
superdoggo83 <bramvandersluis83@gmail.com>
2024-01-17 13:52:55 +0100

    jsepic

--- jscode/formvalidate.js
+++ jscode/formvalidate.js
@@ -1,19 +1,42 @@
-function naamcheck(){
-  if(  document.getElementById("voornaam").value.length < 2){
-    document.getElementById("results").innerHTML = "uw voornaam moet minimaal 2 karakters bevatten"
+// function naamcheck(){
+//   if(  document.getElementById("voornaam").value.length < 2){
+//     document.getElementById("results").innerHTML += "uw voornaam moet minimaal 2 karakters bevatten"
+//   }
+//     else{
+//       document.getElementById("results").innerHTML = " "
+//     }
+// }
+
+// function anaamcheck(){
+//   if(  document.getElementById("voornaam").value.length < 2){
+//     document.getElementById("results").innerHTML += "uw achternaam moet minimaal 2 karakters bevatten"
+//   }
+//     else{
+//       document.getElementById("results").innerHTML = " "
+//     }
+// }
+
+function errorform() {
+  let errormsg = new Array();
+  if (document.getElementById("voornaam").value.length < 2) {
+    errormsg.push("uw voornaam moet minimaal 2 karakters bevatten");
   }
-    else{
-      document.getElementById("results").innerHTML = " "
-    }
-}
-
-
-function anaamcheck(){
-  if(  document.getElementById("voornaam").value.length < 2){
-    document.getElementById("results").innerHTML = "uw achternaam moet minimaal 2 karakters bevatten"
+  if (document.getElementById("achternaam").value.length < 2) {
+    errormsg.push("uw achternaam moet minimaal 2 karakters bevatten");
   }
-    else{
-      document.getElementById("results").innerHTML = " "
-    }
-}
+  if (document.getElementById("adres").value.length < 2) {
+    errormsg.push("uw adres moet minimaal 2 karakters bevatten");
+  }
+  if (document.getElementById("postcode").value.length < 6) {
+    errormsg.push("uw postcode moet minimaal 6 karakters bevatten");
+  }
+  if (!document.getElementById("plaats").value.length < 2) {
+    errormsg.push("uw plaatsnaam moet minimaal 2 karakters bevatten");
+  }
 
+  let errorlist = "";
+  errormsg.forEach(function (message) {
+    errorlist += "<li>" + message + "</li>";
+  });
+  document.getElementById("results").innerHTML = errorlist;
+}
